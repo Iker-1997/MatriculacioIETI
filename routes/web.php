7f40b918bd0608cef83ecfaa9e07be6ad0524ceb
Pradas137 <pradas137@gmail.com>
2021-03-05 15:58:51 +0100

    roles 5 de marzo 15:58

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin_dashboard', 'Admin\DashboardController@index')->middleware('role:admin');
+Route::get('/seller_dashboard', 'Seller\DashboardController@index')->middleware('role:seller');
