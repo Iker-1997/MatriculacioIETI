c185270068458b2263d51cf231ba40b9702fb976
JesusSePe <umbreon20019@gmail.com>
2021-03-07 19:50:35 +0100

    Logs table correction

--- database/migrations/2021_03_04_173129_proyecto_migrate.php
+++ database/migrations/2021_03_04_173129_proyecto_migrate.php
@@ -126,7 +126,7 @@ class ProyectoMigrate extends Migration{
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('lavel');
+            $table->string('level');
             $table->text('message');
             $table->timestamps();
         });
