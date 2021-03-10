20f91e8be8e46cfdabcb7e24a4da2dd31bb58028
Pradas137 <pradas137@gmail.com>
2021-03-10 21:40:53 +0100

    roles

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
