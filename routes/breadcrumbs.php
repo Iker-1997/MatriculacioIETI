a650e4fe60978ba54739433eb16d169b122c214b
Pradas137 <pradas137@gmail.com>
2021-03-13 11:44:41 +0100

    mejoras

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,8 +4,18 @@ use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
 Breadcrumbs::for('dashboard', function ($trail) {
     $trail->push('ControlPanel', route('dashboard'));
 });
-// Inicio > Generos
+// Inicio > Cursos
 Breadcrumbs::for('terms', function ($trail) {
     $trail->parent('dashboard');
     $trail->push('Terms', route('terms'));
+});
+// Inicio > Cursos > [Cycles]
+Breadcrumbs::for('Cycles', function ($trail, $cycle) {
+    $trail->parent('Cycles');
+    $trail->push($cycle->name, route('Cycles.show', $cycle));
+});
+// Inicio > Cursos > [Cycles] > [MPs]
+Breadcrumbs::for('mps', function ($trail, $mps) {
+    $trail->parent('Cycles', $mps->cycle);
+    $trail->push($cycle->name, route('Cycles.show', $cycle));
 });
