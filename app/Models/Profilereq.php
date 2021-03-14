6b1bea73ac954192f054da522dc1283672aaf1be
JesusSePe <umbreon20019@gmail.com>
2021-03-14 19:42:41 +0100

    Soft delete enabled on profilereq

--- app/Models/Profilereq.php
+++ app/Models/Profilereq.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+// ------- SOFT DELETE ----------
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use App\Models\Requirements;
 
 class Profilereq extends Model
 {
     use HasFactory;
+
+    // Enabling soft delete on model
+    use SoftDeletes;
 
     public function requirements() {
         return $this->hasMany(Requirements::class);
